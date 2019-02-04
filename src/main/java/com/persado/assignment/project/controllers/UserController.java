516ad35483f51cefeda0963f3ae8f5b7900b5559
GiorgosAnagnostakis <g.anagnostakis@hotmail.com>
2019-02-04 14:35:18 +0200

    Added manage users with delete button No control if User has a book on loan YET

--- src/main/java/com/persado/assignment/project/controllers/UserController.java
+++ src/main/java/com/persado/assignment/project/controllers/UserController.java
@@ -11,6 +11,7 @@ import com.persado.assignment.project.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,16 +29,42 @@ public class UserController {
 
 
     @RequestMapping(path = "/createNewUser", method = RequestMethod.GET)
-    public String createNewBook(Model model) {
+    public String createNewUser(Model model) {
         model.addAttribute("user", new User());
         return "createNewUser";
     }
 
     @RequestMapping(path = "/createNewUser", method = RequestMethod.POST)
-    public String saveNewBook(User user){
+    public String saveNewUser(User user){
         userRepository.save(user);
         return "redirect:/";
     }
 
-   
+
+
+
+
+    @RequestMapping(path = "/manageUsers", method = RequestMethod.GET)
+    public String getBooks(Model model) {
+
+        model.addAttribute("users", userRepository.findAll());
+        //   model.addAttribute("users", userRepository.findAll());
+        //System.out.println(loanRepository.findAll());
+        //long id = 1;
+       // loanRepository.countByIdEquals(id);
+        //System.out.println(loanRepository.countByIdEquals(id));
+       // System.out.println(loanRepository.countByIdEqualsAndReturnDateIsNotNull(id));
+        return "manageUsers";
+    }
+
+
+    @RequestMapping(path = "/manageUsers/delete/{id}", method = RequestMethod.GET)
+    public String deleteProduct(@PathVariable(name = "id") Long id) {
+        userRepository.deleteById(id);
+        return "redirect:/manageUsers";
+    }
+
+
+
+
 }
