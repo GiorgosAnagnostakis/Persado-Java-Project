c59676dfaa27d7274bedda53e1c83973dda17368
GiorgosAnagnostakis <g.anagnostakis@hotmail.com>
2019-02-05 00:59:03 +0200

    disabled Spring Security Login Page (Task 2)

--- src/main/java/com/persado/assignment/project/ProjectApplication.java
+++ src/main/java/com/persado/assignment/project/ProjectApplication.java
@@ -2,13 +2,36 @@ package com.persado.assignment.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ProjectApplication {
+@EnableWebSecurity
+public class ProjectApplication extends WebSecurityConfigurerAdapter {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+
+		new SpringApplicationBuilder(ProjectApplication.class)
+				.properties("server.port=8092","management.port=8000","management.security.enabled=false")
+				.run(args);
+	}
+
+
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests().anyRequest().authenticated()
+				.antMatchers( "/**").authenticated()
+				.anyRequest().permitAll()
+		;
+
 	}
 
 
